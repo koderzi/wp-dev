9e5ac867b926cd9cf5c466ad738631d9d71a884b
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 07:34:39 +0800

    fix

--- .devcontainer/dev-load.sh
+++ .devcontainer/dev-load.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-for d in /workspaces/*/ ; do 
-    dir=$(basename $d)
-    if [ -L /var/www/html/wp-content/plugins/$dir ] && [ $(readlink -f /var/www/html/wp-content/plugins/$dir) != $d ]; then 
-        rm /var/www/html/wp-content/plugins/$dir
-    fi 
-    if [ ! -L /var/www/html/wp-content/plugins/$dir ]; then 
-        ln -s $d /var/www/html/wp-content/plugins/$dir
+for w in /workspaces/*/ ; do
+    if [ ! -d "$w/.devcontainer" ]; then
+        continue
     fi
+    for p in $w/plugins/*/ ; do
+        plugin=$(basename $p)
+        if [ -L /var/www/html/wp-content/plugins/$plugin ]; then 
+            rm /var/www/html/wp-content/plugins/$plugin
+        fi 
+        if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
+            ln -s $p /var/www/html/wp-content/plugins/$plugin
+        fi
+    done
+    for t in $w/themes/*/ ; do 
+        theme=$(basename $t)
+        if [ -L /var/www/html/wp-content/themes/$theme ]; then 
+            rm /var/www/html/wp-content/themes/$theme
+        fi 
+        if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
+            ln -s $t /var/www/html/wp-content/themes/$theme
+        fi
+    done
 done
