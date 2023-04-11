d25e7fe6cc3eff1c14982f6d45a45b92ab986bbd
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 09:28:10 +0800

    fix

--- .devcontainer/dev-embed.sh
+++ .devcontainer/dev-embed.sh
@@ -2,24 +2,24 @@ for w in /workspaces/*/ ; do
     echo "workspace folder $w\n";
     if [ ! -d $w/.devcontainer ]; then
         echo "devcontainer folder not found, skip\n";
-        continue
-    fi
+        continue;
+    fi;
     for p in $w/plugins/*/ ; do
-        plugin=$(basename $p)
+        plugin=$(basename $p);
         if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
-            rm /var/www/html/wp-content/plugins/$plugin
+            rm /var/www/html/wp-content/plugins/$plugin;
         fi 
         if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
-            ln -s $p /var/www/html/wp-content/plugins/$plugin
+            ln -s $p /var/www/html/wp-content/plugins/$plugin;
         fi
     done
     for t in $w/themes/*/ ; do 
-        theme=$(basename $t)
+        theme=$(basename $t);
         if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) != $t ]; then
-            rm /var/www/html/wp-content/themes/$theme
-        fi 
+            rm /var/www/html/wp-content/themes/$theme;
+        fi;
         if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
-            ln -s $t /var/www/html/wp-content/themes/$theme
-        fi
+            ln -s $t /var/www/html/wp-content/themes/$theme;
+        fi;
     done
 done
