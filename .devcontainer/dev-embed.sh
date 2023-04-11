9cc1ba9068934f497a836e04bd445e1f390a152e
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 09:14:38 +0800

    fix

--- .devcontainer/dev-embed.sh
+++ .devcontainer/dev-embed.sh
@@ -1,18 +1,25 @@
-for p in ./plugins/*/ ; do
-    plugin=$(basename $p)
-    if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
-        rm /var/www/html/wp-content/plugins/$plugin
-    fi 
-    if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
-        ln -s $p /var/www/html/wp-content/plugins/$plugin
+for w in /workspaces/*/ ; do
+    echo "workspace folder $w\n";
+    if [ ! -d $w/.devcontainer ]; then
+        echo "devcontainer folder not found, skip\n";
+        continue
     fi
-done
-for t in ./themes/*/ ; do 
-    theme=$(basename $t)
-    if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) != $t ]; then
-        rm /var/www/html/wp-content/themes/$theme
-    fi 
-    if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
-        ln -s $t /var/www/html/wp-content/themes/$theme
-    fi
+    for p in $w/plugins/*/ ; do
+        plugin=$(basename $p)
+        if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
+            rm /var/www/html/wp-content/plugins/$plugin
+        fi 
+        if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
+            ln -s $p /var/www/html/wp-content/plugins/$plugin
+        fi
+    done
+    for t in $w/themes/*/ ; do 
+        theme=$(basename $t)
+        if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) != $t ]; then
+            rm /var/www/html/wp-content/themes/$theme
+        fi 
+        if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
+            ln -s $t /var/www/html/wp-content/themes/$theme
+        fi
+    done
 done
