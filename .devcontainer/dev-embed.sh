5896638db286c535492fdd6b4c181db2d85ce9b4
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 08:25:06 +0800

    fix

--- .devcontainer/dev-embed.sh
+++ .devcontainer/dev-embed.sh
@@ -1,23 +1,18 @@
-for w in /workspaces/*/ ; do
-    if [ ! -d "$w/.devcontainer" ]; then
-        continue
+for p in ./plugins/*/ ; do
+    plugin=$(basename $p)
+    if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
+        rm /var/www/html/wp-content/plugins/$plugin
+    fi 
+    if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
+        ln -s $p /var/www/html/wp-content/plugins/$plugin
     fi
-    for p in $w/plugins/*/ ; do
-        plugin=$(basename $p)
-        if [ -L /var/www/html/wp-content/plugins/$plugin ]; then 
-            rm /var/www/html/wp-content/plugins/$plugin
-        fi 
-        if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
-            ln -s $p /var/www/html/wp-content/plugins/$plugin
-        fi
-    done
-    for t in $w/themes/*/ ; do 
-        theme=$(basename $t)
-        if [ -L /var/www/html/wp-content/themes/$theme ]; then 
-            rm /var/www/html/wp-content/themes/$theme
-        fi 
-        if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
-            ln -s $t /var/www/html/wp-content/themes/$theme
-        fi
-    done
+done
+for t in ./themes/*/ ; do 
+    theme=$(basename $t)
+    if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) != $t ]; then
+        rm /var/www/html/wp-content/themes/$theme
+    fi 
+    if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
+        ln -s $t /var/www/html/wp-content/themes/$theme
+    fi
 done
