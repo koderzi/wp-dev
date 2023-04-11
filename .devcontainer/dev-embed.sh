1e45b19fb54c7fa33bd5373eca3b258cf6b512e8
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 14:02:07 +0800

    fix

--- .devcontainer/dev-embed.sh
+++ .devcontainer/dev-embed.sh
@@ -1,10 +1,9 @@
 for w in /workspaces/*/ ; do
-    echo "workspace folder $w\n";
     if [ ! -d $w/.devcontainer ]; then
-        echo "devcontainer folder not found, skip\n";
         continue;
     fi;
     for p in $w/plugins/*/ ; do
+        echo "plugin: $p";
         plugin=$(basename $p);
         if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) -ne $p ]; then
             rm /var/www/html/wp-content/plugins/$plugin;
