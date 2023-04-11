5beee5c0bf5f541a911f54a0b7396102a6bb000c
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 14:58:39 +0800

    fix

--- .devcontainer/dev-embed.sh
+++ .devcontainer/dev-embed.sh
@@ -1,24 +1,29 @@
-for w in /workspaces/*/ ; do
-    if [ ! -d $w/.devcontainer ]; then
+for d in /workspaces/*/ ; do
+    if [ ! -d $d/.devcontainer ]; then
         continue;
     fi;
-    for p in $w/plugins/*/ ; do
-        echo "plugin: $p";
-        plugin=$(basename $p);
-        if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) -ne $p ]; then
-            rm /var/www/html/wp-content/plugins/$plugin;
-        fi 
-        if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
-            ln -s $p /var/www/html/wp-content/plugins/$plugin;
-        fi
-    done
-    for t in $w/themes/*/ ; do 
-        theme=$(basename $t);
-        if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) -ne $t ]; then
-            rm /var/www/html/wp-content/themes/$theme;
-        fi;
-        if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
-            ln -s $t /var/www/html/wp-content/themes/$theme;
-        fi;
-    done
+    if [ -d $d/.devcontainer ]; then
+        dir=$(basename $d);
+        break;
+    fi;
+done
+plugins=/workspaces/$dir/plugins/*/
+for p in $plugins ; do
+    plugin=$(basename $p);
+    if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
+        rm /var/www/html/wp-content/plugins/$plugin;
+    fi 
+    if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
+        ln -s $p /var/www/html/wp-content/plugins/$plugin;
+    fi
+done
+themes=/workspaces/$dir/themes/*/
+for t in $themes ; do
+    theme=$(basename $p);
+    if [ -L /var/www/html/wp-content/plugins/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) != $t ]; then
+        rm /var/www/html/wp-content/plugins/$theme;
+    fi 
+    if [ ! -L /var/www/html/wp-content/plugins/$theme ]; then 
+        ln -s $t /var/www/html/wp-content/plugins/$theme;
+    fi
 done
