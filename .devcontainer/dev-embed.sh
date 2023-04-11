2f2b39762b7e35bbc89cf4533859aaa75914ad6e
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-04-11 11:26:58 +0800

    fix

--- .devcontainer/dev-embed.sh
+++ .devcontainer/dev-embed.sh
@@ -6,7 +6,7 @@ for w in /workspaces/*/ ; do
     fi;
     for p in $w/plugins/*/ ; do
         plugin=$(basename $p);
-        if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
+        if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) -ne $p ]; then
             rm /var/www/html/wp-content/plugins/$plugin;
         fi 
         if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
@@ -15,7 +15,7 @@ for w in /workspaces/*/ ; do
     done
     for t in $w/themes/*/ ; do 
         theme=$(basename $t);
-        if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) != $t ]; then
+        if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/plugins/$theme) -ne $t ]; then
             rm /var/www/html/wp-content/themes/$theme;
         fi;
         if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
