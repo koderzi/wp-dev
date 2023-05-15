202e2bd1ccafc08f77e72ded773446ee9e9004c2
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-05-15 19:05:29 +0800

    update

--- .devcontainer/devcontainer.sh
+++ .devcontainer/devcontainer.sh
@@ -18,8 +18,51 @@ fi
 
 echo "Git username and email have been configured."
 
-# create a cron to run devcron.sh file every minute
-crontab -l > cron.txt
-echo "*/1 * * * * $PWD/devcron.sh" >> cron.txt
-crontab cron.txt
-rm cron.txt
+# Function to execute the script
+function run_script {
+  # Attach plugins folders to the workspace
+  plugins=$(dirname $PWD)/plugins/*/
+  for p in $plugins ; do
+      plugin=$(basename $p);
+      if [ "$plugin" = "*" ]; then
+          continue;
+      fi;
+      if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
+          rm /var/www/html/wp-content/plugins/$plugin;
+      fi 
+      if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
+          ln -s $p /var/www/html/wp-content/plugins/$plugin;
+      fi
+  done
+  # Attach themes folders to the workspace
+  themes=$(dirname $PWD)/themes/*/
+  for t in $themes ; do
+      theme=$(basename $t);
+      if [ "$theme" = "*" ]; then
+          continue;
+      fi;
+      if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/themes/$theme) != $t ]; then
+          rm /var/www/html/wp-content/themes/$theme;
+      fi 
+      if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
+          ln -s $t /var/www/html/wp-content/themes/$theme;
+          chown -R www-data:www-data /workspaces/$dir/themes;
+      fi
+  done
+  find /var/www/html/wp-content/plugins/ -type l ! -exec test -e {} \; -delete
+  find /var/www/html/wp-content/themes/ -type l ! -exec test -e {} \; -delete
+
+  # Change ownership of files and directories inside plugins and themes folders
+  chown -R www-data:www-data /var/www/html/wp-content/plugins
+  chown -R www-data:www-data /var/www/html/wp-content/themes
+  chown -R www-data:www-data $(dirname $PWD)/plugins
+  chown -R www-data:www-data $(dirname $PWD)/themes
+
+  echo "Plugins and themes folders have been linked and ownership has been changed."
+}
+
+# Watch for changes in plugins and themes directory
+while inotifywait -r -e modify,create,delete,move $(dirname $PWD)/plugins $(dirname $PWD)/themes; do
+  # Execute the script
+  run_script
+done
