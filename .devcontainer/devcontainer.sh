c7f23c911a4f6ecd0bfb139afe351acff2d7f252
fiziechemi <fiziechemi@gmail.com>
2023-05-15 07:44:42 +0000

    update

--- .devcontainer/devcontainer.sh
+++ .devcontainer/devcontainer.sh
@@ -18,50 +18,8 @@ fi
 
 echo "Git username and email have been configured."
 
-# Attach plugins & themes folders
-for d in /workspaces/*/ ; do
-    if [ ! -d $d/.devcontainer ]; then
-        continue;
-    fi;
-    if [ -d $d/.devcontainer ]; then
-        dir=$(basename $d);
-        break;
-    fi;
-done
-plugins=/workspaces/$dir/plugins/*/
-for p in $plugins ; do
-    plugin=$(basename $p);
-    if [ "$plugin" = "*" ]; then
-        continue;
-    fi;
-    if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
-        rm /var/www/html/wp-content/plugins/$plugin;
-    fi 
-    if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
-        ln -s $p /var/www/html/wp-content/plugins/$plugin;
-        chown -R www-data:www-data /workspaces/$dir/plugins;
-    fi
-done
-themes=/workspaces/$dir/themes/*/
-for t in $themes ; do
-    theme=$(basename $t);
-    if [ "$theme" = "*" ]; then
-        continue;
-    fi;
-    if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/themes/$theme) != $t ]; then
-        rm /var/www/html/wp-content/themes/$theme;
-    fi 
-    if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
-        ln -s $t /var/www/html/wp-content/themes/$theme;
-        chown -R www-data:www-data /workspaces/$dir/themes;
-    fi
-done
-find /var/www/html/wp-content/plugins/ -type l ! -exec test -e {} \; -delete
-find /var/www/html/wp-content/themes/ -type l ! -exec test -e {} \; -delete
-
-# Change ownership of files and directories inside plugins and themes folders
-
-chown -R www-data:www-data /var/www/html/wp-content/themes
-chown -R www-data:www-data /var/www/html/wp-content/plugins
-
-echo "Plugins and themes folders have been linked and ownership has been changed."
+# create a cron to run devcron.sh file every minute
+crontab -l > cron.txt
+echo "*/1 * * * * $PWD/devcron.sh" >> cron.txt
+crontab cron.txt
+rm cron.txt
