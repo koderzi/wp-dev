de6735de022c083627d19a0aa9b0c31b6a7478c2
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-05-23 11:14:20 +0800

    update

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sh "$PWD"/.devcontainer/setup_folder.sh & disown
-sh "$PWD"/.devcontainer/setup_git.sh;
+sh "$PWD"/.devcontainer/setup_folder.sh &
+sh "$PWD"/.devcontainer/setup_git.sh
