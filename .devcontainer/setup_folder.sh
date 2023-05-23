b14810003fe5e826853fc7e4515d9859d50a17e4
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-05-23 11:05:37 +0800

    update

--- .devcontainer/setup_folder.sh
+++ .devcontainer/setup_folder.sh
@@ -1,51 +1,51 @@
 #!/bin/sh
 
-# Function to execute the script
-setup_folder() {
-    # Attach plugins folders to the workspace
-    plugins=$(dirname "$PWD")/plugins/*/
-    for p in $plugins ; do
-        plugin=$(basename $p);
+# Function to set up the workspace
+setup_workspace() {
+    WP_DIR=$(dirname $(dirname "$(readlink -f "$0")"))
+    # Attach plugin folders to the workspace
+    for plugin_dir in $WP_DIR/plugins/*/ ; do
+        plugin=$(basename $plugin_dir);
         if [ "$plugin" = "*" ]; then
             continue;
         fi;
-        if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $p ]; then
+        if [ -L /var/www/html/wp-content/plugins/$plugin ] && [ $(readlink -f /var/www/html/wp-content/plugins/$plugin) != $plugin_dir ]; then
             rm /var/www/html/wp-content/plugins/$plugin;
         fi 
         if [ ! -L /var/www/html/wp-content/plugins/$plugin ]; then 
-            ln -s $p /var/www/html/wp-content/plugins/$plugin;
+            ln -s $plugin_dir /var/www/html/wp-content/plugins/$plugin;
         fi
     done
-    # Attach themes folders to the workspace
-    themes=$wp_folder/themes/*/
-    for t in $themes ; do
-        theme=$(basename $t);
+    # Attach theme folders to the workspace
+    for theme_dir in $WP_DIR/themes/*/ ; do
+        theme=$(basename $theme_dir);
         if [ "$theme" = "*" ]; then
             continue;
         fi;
-        if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/themes/$theme) != $t ]; then
+        if [ -L /var/www/html/wp-content/themes/$theme ] && [ $(readlink -f /var/www/html/wp-content/themes/$theme) != $theme_dir ]; then
             rm /var/www/html/wp-content/themes/$theme;
         fi 
         if [ ! -L /var/www/html/wp-content/themes/$theme ]; then 
-            ln -s $t /var/www/html/wp-content/themes/$theme;
+            ln -s $theme_dir /var/www/html/wp-content/themes/$theme;
         fi
     done
+    # Remove broken symlinks
     find /var/www/html/wp-content/plugins/ -type l ! -exec test -e {} \; -delete
     find /var/www/html/wp-content/themes/ -type l ! -exec test -e {} \; -delete
 
     # Change ownership of files and directories inside plugins and themes folders
     chown -R www-data:www-data /var/www/html/wp-content/plugins
     chown -R www-data:www-data /var/www/html/wp-content/themes
-    chown -R www-data:www-data $(dirname "$PWD")/plugins
-    chown -R www-data:www-data $(dirname "$PWD")/themes
+    chown -R www-data:www-data $WP_DIR/plugins
+    chown -R www-data:www-data $WP_DIR/themes
 }
 
-setup_folder
+setup_workspace
 
+# Watch for changes in plugins and themes directory
 while true; do
-    # Watch for changes in plugins and themes directory
-    while inotifywait -q -e create,delete,move $(dirname "$PWD")/plugins $(dirname "$PWD")/themes; do
-        # Execute the script
-        setup_folder
-    done
+    WP_DIR=$(dirname $(dirname "$(readlink -f "$0")"))
+    inotifywait -q -e create,delete,move $WP_DIR/plugins $WP_DIR/themes
+    # Execute the script
+    setup_workspace
 done
