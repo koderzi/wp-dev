926e20b6ac29398b6baebb44d3fcf3cf1f91a615
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-05-28 11:08:23 +0800

    update

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Get the current time and add 5 seconds to it
-future=$(date -d '+ 5 seconds' '+%M %H %d %m %u')
+WP_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
+
+echo $WP_DIR
 
 # Set the cronjob to run a command at the future time
-crontab "$future sh $PWD/.devcontainer/setup_folder.sh"
+# crontab "* * * * * sh $WP_DIR/.devcontainer/setup_folder.sh"
 
-sh "$PWD"/.devcontainer/setup_git.sh
+# sh "$WP_DIR"/.devcontainer/setup_git.sh
