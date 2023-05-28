e3e28422880a123282414b9a9c085eda92214eb0
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-05-28 12:01:46 +0800

    update

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -2,7 +2,7 @@
 
 DEV_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 
-SETUPFOLDER = "$DEV_DIR"/.devcontainer/setup_git.sh
+SETUPFOLDER = "$DEV_DIR"/.devcontainer/setup_folder.sh
 crontab "* * * * * sh $SETUPFOLDER"
 
 SETUPGIT = "$DEV_DIR"/.devcontainer/setup_git.sh
