10fa43102b73afd193d23c5c4c45f002d0d1016f
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-05-28 11:52:12 +0800

    update

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -3,6 +3,6 @@
 WP_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 
 # Set the cronjob to run a command at the future time
-crontab "* * * * * sh $WP_DIR/.devcontainer/setup_folder.sh"
+# crontab "* * * * * sh $WP_DIR/.devcontainer/setup_folder.sh"
 
-# sh "$WP_DIR"/.devcontainer/setup_git.sh
+sh "$WP_DIR"/.devcontainer/setup_git.sh
