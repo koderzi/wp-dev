25202189adbd76c015168ea8ec95b310dbb2dacb
FizieCheMi <30676109+fiziechemi@users.noreply.github.com>
2023-06-09 04:39:33 -0700

    update

--- .devcontainer/setup_folder.php
+++ .devcontainer/setup_folder.php
@@ -5,12 +5,10 @@ define('WP_DIR', dirname(__DIR__));
 // Function to set up the workspace
 function setup_workspace()
 {
-    $WP_DIR = WP_DIR;
-
     // Attach plugin folders to the workspace
-    $plugins = scandir("{$WP_DIR}/plugins");
+    $plugins = scandir(WP_DIR . "/plugins");
     foreach ($plugins as $plugin) {
-        $plugin_dir = "{$WP_DIR}/plugins/{$plugin}";
+        $plugin_dir = WP_DIR . "/plugins/{$plugin}";
         if ($plugin === '.' || $plugin === '..' || is_file($plugin_dir)) {
             continue;
         }
@@ -23,9 +21,9 @@ function setup_workspace()
     }
 
     // Attach theme folders to the workspace
-    $themes = scandir("{$WP_DIR}/themes");
+    $themes = scandir(WP_DIR . "/themes");
     foreach ($themes as $theme) {
-        $theme_dir = "{$WP_DIR}/plugins/{$theme}";
+        $theme_dir = WP_DIR . "/plugins/{$theme}";
         if ($theme === '.' || $theme === '..' || is_file($theme_dir)) {
             continue;
         }
@@ -44,21 +42,28 @@ function setup_workspace()
     // Change ownership of files and directories inside plugins and themes folders
     exec('chown -R www-data:www-data /var/www/html/wp-content/plugins');
     exec('chown -R www-data:www-data /var/www/html/wp-content/themes');
-    exec("chown -R www-data:www-data {$WP_DIR}/plugins");
-    exec("chown -R www-data:www-data {$WP_DIR}/themes");
+    exec("chown -R www-data:www-data " . WP_DIR . "/plugins");
+    exec("chown -R www-data:www-data " . WP_DIR . "/themes");
 }
 
 function list_plugins()
 {
-    $WP_DIR = WP_DIR;
-    return array_values(array_diff(scandir("{$WP_DIR}/plugins"), ['.', '..']));
+    return array_values(array_diff(scandir(WP_DIR . "/plugins"), ['.', '..']));
+}
+
+function list_themes()
+{
+    return array_values(array_diff(scandir(WP_DIR . "/themes"), ['.', '..']));
 }
 
 $list_plugins = [];
+$list_themes = [];
 do {
     $new_list_plugins = list_plugins();
-    if ($new_list_plugins !== $list_plugins) {
+    $new_list_themes = list_themes();
+    if ($new_list_plugins !== $list_plugins || $new_list_themes !== $list_themes) {
         $list_plugins = $new_list_plugins;
+        $list_themes = $new_list_themes;
         setup_workspace();
     }
     sleep(1);
