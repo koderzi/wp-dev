849ae3bddb08ecaf27cf75ee2b379eec5cccafd2
fiziechemi <fiziechemi@gmail.com>
2023-06-19 03:35:14 +0000

    update init-container.php

--- .devcontainer/init-container.php
+++ .devcontainer/init-container.php
@@ -11,15 +11,12 @@ if ($argv[1] == 'install') {
 }
 
 if ($argv[1] == 'setup') {
-    if (file_exists('/setup.bak')) {
-        $exec = "nohup /usr/local/bin/php " . __DIR__ . "/container/setup_folder.php > /dev/null 2>&1 &";
-        exec($exec);
-        echo ("\nConfigured directory.\n");
-        include_once(__DIR__ . "/container/setup_git.php");
-        include_once(__DIR__ . "/container/setup_xdebug.php");
-    }
-    exec('echo "" > /setup.bak');
-    if (AUTO_UPDATE) {
+    $exec = "nohup /usr/local/bin/php " . __DIR__ . "/container/setup_folder.php > /dev/null 2>&1 &";
+    exec($exec);
+    echo ("\nConfigured directory.\n");
+    include_once(__DIR__ . "/container/setup_git.php");
+    include_once(__DIR__ . "/container/setup_xdebug.php");
+    if (file_exists('/xdebug.bak') && strlen(file_get_contents('/xdebug.bak')) == 1 && AUTO_UPDATE) {
         update();
     }
     echo "\n";
@@ -30,10 +27,11 @@ function update()
     echo "\nUpdating container...\n";
     $cwd = getcwd();
     chdir(__DIR__);
+    $token = base64_decode('Z2l0aHViX3BhdF8xMUFIS0JKREkwdXJtU2pmNzdNbmFiX0ZnNmNhek45Q3dJSWhMbmIxVlk1V2hMNld2eEhQVm95ZU4yZXVxcGlMR2I0TDJITkVXTzlOOWNGZGFz');
     new Updater(
         'koderzi',
         'wp-dev',
-        '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',
+        $token,
         '1.0.2',
         '',
         '',
