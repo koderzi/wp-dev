0d7a784ade0650ca93b00394b668e8825c280c2f
koderzi <30676109+koderzi@users.noreply.github.com>
2023-06-19 09:57:15 +0800

    update

--- .devcontainer/init-container.php
+++ .devcontainer/init-container.php
@@ -8,10 +8,10 @@ use KoderZi\PhpGitHubUpdater\Updater;
 
 if ($argv[1] == 'install') {
     include_once(__DIR__ . "/container/install_extension.php");
-    update();
-    echo "\n\033[1mActivating container update...\033[0m\n";
-    echo "\033[1mPress 'Reload Window' when prompted.\033[0m\n";
-    exec("nohup apache2ctl -k restart > /dev/null 2>&1 &");
+    // update();
+    // echo "\n\033[1mActivating container update...\033[0m\n";
+    // echo "\033[1mPress 'Reload Window' when prompted.\033[0m\n";
+    // exec("nohup apache2ctl -k restart > /dev/null 2>&1 &");
 }
 
 if ($argv[1] == 'setup') {
@@ -22,7 +22,7 @@ if ($argv[1] == 'setup') {
         include_once(__DIR__ . "/container/setup_git.php");
         include_once(__DIR__ . "/container/setup_xdebug.php");
         if (file_exists('/xdebug.bak') && strlen(file_get_contents('/xdebug.bak')) > 1  && AUTO_UPDATE) {
-            update();
+            // update();
         }
         echo "\n";
     }
