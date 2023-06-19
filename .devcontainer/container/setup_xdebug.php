04b950f6625578386a9d5758043ed56b9716125e
fiziechemi <fiziechemi@gmail.com>
2023-06-19 01:02:14 +0000

    update setup_xdebug.php and init-container.php

--- .devcontainer/container/setup_xdebug.php
+++ .devcontainer/container/setup_xdebug.php
@@ -8,13 +8,11 @@ if (!file_exists('/usr/local/etc/php/conf.d/xdebug.ini')) {
 echo "xdebug configured.\n";
 
 if (file_exists('/xdebug.bak')) {
-    exec('echo "activated" > /xdebug.bak');
+    exec('echo "" > /xdebug.bak');
     return;
 }
 
-echo "\nActivating git and xdebug...\n";
-echo "\033[1mPress 'Reload Window' when prompted.\033[0m\n";
+echo "\nActivating git and xdebug...\n\033[1mPress 'Reload Window' when prompted.\033[0m\n";
 
 exec("nohup apache2ctl -k restart > /dev/null 2>&1 &");
-
 exec('echo "activating" > /xdebug.bak');
--- .devcontainer/init-container.php
+++ .devcontainer/init-container.php
@@ -8,34 +8,47 @@ use KoderZi\PhpGitHubUpdater\Updater;
 
 if ($argv[1] == 'install') {
     include_once(__DIR__ . "/container/install_extension.php");
+    update();
+    if (!file_exists('/setup.bak')) {
+        echo "\nActivating container update...\n";
+        echo "\033[1mPress 'Reload Window' when prompted.\033[0m\n";
+        exec("nohup apache2ctl -k restart > /dev/null 2>&1 &");
+    }
 }
 
 if ($argv[1] == 'setup') {
-    $exec = "nohup /usr/local/bin/php " . __DIR__ . "/container/setup_folder.php > /dev/null 2>&1 &";
-    exec($exec);
-    echo ("\ndirectory configured.\n");
-    include_once(__DIR__ . "/container/setup_git.php");
-    include_once(__DIR__ . "/container/setup_xdebug.php");
-
-    if (file_exists('/xdebug.bak') && file_get_contents('/xdebug.bak') == 'activated' && AUTO_UPDATE) {
-        $cwd = getcwd();
-        chdir(__DIR__);
-        new Updater(
-            'koderzi',
-            'wp-dev',
-            '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',
-            '1.0.1',
-            '',
-            '',
-            [
-                'file' => [
-                    'docker-compose.yaml',
-                    'devcontainer.json'
-                ]
-            ]
-        );
-        chdir($cwd);
-        echo "container updated.\n";
+    if (file_exists('/setup.bak')) {
+        $exec = "nohup /usr/local/bin/php " . __DIR__ . "/container/setup_folder.php > /dev/null 2>&1 &";
+        exec($exec);
+        echo ("\ndirectory configured.\n");
+        include_once(__DIR__ . "/container/setup_git.php");
+        include_once(__DIR__ . "/container/setup_xdebug.php");
+        if (file_exists('/xdebug.bak') && strlen(file_get_contents('/xdebug.bak')) > 1  && AUTO_UPDATE) {
+            update();
+        }
+        echo "\n";
     }
-    echo "\n";
+    exec('echo "" > /setup.bak');
+}
+
+function update()
+{
+    $cwd = getcwd();
+    chdir(__DIR__);
+    new Updater(
+        'koderzi',
+        'wp-dev',
+        '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',
+        '1.0.2',
+        '',
+        '',
+        [
+            'file' => [
+                'docker-compose.yaml',
+                'devcontainer.json'
+            ]
+        ]
+    );
+    chdir($cwd);
+    echo "container updated.\n";
 }
