242f916b3eb86fe36fdd8a18d36b040941cac429
fiziechemi <fiziechemi@gmail.com>
2023-06-19 03:54:05 +0000

    update setup_xdebug.php

--- .devcontainer/container/setup_xdebug.php
+++ .devcontainer/container/setup_xdebug.php
@@ -12,7 +12,7 @@ if (file_exists('/xdebug.bak')) {
     return;
 }
 
-echo "\nActivating git and xdebug...\n\033[1mPress 'Reload Window' when prompted.\033[0m\n";
+echo "\n\033[1mActivating git and xdebug...\033[0m\n\n\033[1mPress 'Reload Window' when prompted.\033[0m\n";
 
 exec("nohup apache2ctl -k restart > /dev/null 2>&1 &");
 exec('echo "activating" > /xdebug.bak');
