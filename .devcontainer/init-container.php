5f80798e624d3c667ced437667b404bf93e3718f
koderzi <30676109+koderzi@users.noreply.github.com>
2023-06-19 10:55:18 +0800

    update

--- .devcontainer/init-container.php
+++ .devcontainer/init-container.php
@@ -8,10 +8,6 @@ use KoderZi\PhpGitHubUpdater\Updater;
 
 if ($argv[1] == 'install') {
     include_once(__DIR__ . "/container/install_extension.php");
-    // update();
-    // echo "\n\033[1mActivating container update...\033[0m\n";
-    // echo "\033[1mPress 'Reload Window' when prompted.\033[0m\n";
-    // exec("nohup apache2ctl -k restart > /dev/null 2>&1 &");
 }
 
 if ($argv[1] == 'setup') {
@@ -21,12 +17,12 @@ if ($argv[1] == 'setup') {
         echo ("\nConfigured directory .\n");
         include_once(__DIR__ . "/container/setup_git.php");
         include_once(__DIR__ . "/container/setup_xdebug.php");
-        if (file_exists('/xdebug.bak') && strlen(file_get_contents('/xdebug.bak')) > 1  && AUTO_UPDATE) {
-            // update();
-        }
-        echo "\n";
     }
     exec('echo "" > /setup.bak');
+    if (AUTO_UPDATE) {
+        update();
+    }
+    echo "\n";
 }
 
 function update()
@@ -34,19 +30,19 @@ function update()
     echo "Updating container...\n";
     $cwd = getcwd();
     chdir(__DIR__);
-    new Updater(
-        'koderzi',
-        'wp-dev',
-        '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',
-        '1.0.2',
-        '',
-        '',
-        [
-            'file' => [
-                'docker-compose.yaml',
-                'devcontainer.json'
-            ]
-        ]
-    );
+    // new Updater(
+    //     'koderzi',
+    //     'wp-dev',
+    //     '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',
+    //     '1.0.2',
+    //     '',
+    //     '',
+    //     [
+    //         'file' => [
+    //             'docker-compose.yaml',
+    //             'devcontainer.json'
+    //         ]
+    //     ]
+    // );
     chdir($cwd);
 }
