2a903410d48c603d552f042ea7c7ae46ccd5cc3e
koderzi <30676109+koderzi@users.noreply.github.com>
2023-06-25 13:17:40 +0800

    update

--- .devcontainer/container/setup_folder.php
+++ .devcontainer/container/setup_folder.php
@@ -1,10 +1,10 @@
 <?php
 
-if (isset($argv[1]) && $argv[1] === 'setup') {
+if (isset($argv[1]) && $argv[1] === 'monitor') {
     include_once(dirname(__DIR__) . "/class/Directory.php");
     new DirectoryManager();
 } else {
-    $exec = "nohup /usr/local/bin/php -n " . __FILE__ . " 'setup' > /dev/null 2>&1 &";
+    $exec = "nohup /usr/local/bin/php -n " . __FILE__ . " 'monitor' > /dev/null 2>&1 &";
     echo ("\nConfiguring directory.\n");
     exec($exec);
     echo ("\nConfigured directory.\n");
