43891bebb71d230784ec8167423ab7307af9c8f1
koderzi <30676109+koderzi@users.noreply.github.com>
2023-07-25 15:36:38 +0800

    update 2 files

--- .devcontainer/container/init-container.php
+++ .devcontainer/container/init-container.php
@@ -5,16 +5,16 @@ define('AUTO_UPDATE', true);
 $reload = false;
 
 if ($argv[1] == 'install') {
-    include_once(__DIR__ . "/container/install_extension.php");
+    include_once(__DIR__ . "/install_extension.php");
 }
 
 if ($argv[1] == 'setup') {
-    include_once(__DIR__ . "/container/setup_folder.php");
-    include_once(__DIR__ . "/container/setup_git.php");
-    include_once(__DIR__ . "/container/setup_xdebug.php");
-    include_once(__DIR__ . "/container/update_container.php");
+    include_once(__DIR__ . "/setup_folder.php");
+    include_once(__DIR__ . "/setup_git.php");
+    include_once(__DIR__ . "/setup_xdebug.php");
+    include_once(__DIR__ . "/update_container.php");
 }
 
 if ($reload) {
-    include_once(__DIR__ . "/container/reload_apache2.php");
+    include_once(__DIR__ . "/reload_apache2.php");
 }
