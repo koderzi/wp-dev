9958c188f049de89994ab46c5773b4599804d8bc
koderzi <30676109+koderzi@users.noreply.github.com>
2023-08-03 09:05:15 +0800

    update

--- .devcontainer/container/reload_apache2.php
+++ .devcontainer/container/reload_apache2.php
@@ -2,5 +2,6 @@
 
 if (file_exists('/xdebug') && file_get_contents('/xdebug')=='reload') {
     exec('echo "activated" > /xdebug.bak');
+    exec('echo "Reloading"');
     exec("nohup service apache2 reload > /dev/null 2>&1 &");
 }
